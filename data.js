9568bc6ac19355644a5f06ce4d348d023d6a582a
nar2p1nk <farrisisqandar@gmail.com>
2022-02-12 19:25:24 +0800

    testing javascript and typescript

---
+++ data.js
@@ -0,0 +1,8 @@
+const sqlite = require('sqlite3')
+const bcrypt = require('bcrypt')
+
+function createUser(username,password){
+}
+
+const db = new sqlite.Database('userData.db')
+
---
+++ data.ts
@@ -0,0 +1,8 @@
+const sqlite = require('sqlite3')
+const bcrypt = require('bcrypt')
+
+function createUser(username:string,password:string){
+}
+
+const db = new sqlite.Database('userData.db')
+
