394d78359c29c51315de7fb6f24d8ae918489629
nar2p1nk <farrisisqandar@gmail.com>
2022-02-13 16:43:16 +0800

    fixed sqlite bug and created multiple users

--- data.js
+++ data.js
@@ -1,8 +1,17 @@
 const sqlite = require('sqlite3')
 const bcrypt = require('bcrypt')
 
+const db = new sqlite.Database('userData.db')
+
 function createUser(username,password){
+    db.run('INSERT INTO user(username,password) VALUES(?,?)',[username,password],(err)=>{
+        if(err){console.error(err.message)}
+        else{console.log('user created!')}
+    })
+    
 }
 
-const db = new sqlite.Database('userData.db')
+createUser('ppop','pillock')
+
+createUser('torvald','pingu')
 
