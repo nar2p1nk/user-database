37b730c608e65fde00cac8ce6978d5e679c50aaa
nar2p1nk <farrisisqandar@gmail.com>
2022-02-13 17:06:33 +0800

    created table todo with foreign key and created function to create todo

--- data.js
+++ data.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt')
 
 const db = new sqlite.Database('userData.db')
 
+
 function createUser(username,password){
     db.run('INSERT INTO user(username,password) VALUES(?,?)',[username,password],(err)=>{
         if(err){console.error(err.message)}
@@ -11,7 +12,14 @@ function createUser(username,password){
     
 }
 
-createUser('ppop','pillock')
+function createTodo(todo,userId){
+    db.run('INSERT INTO todo(todoName,userId) VALUES(?,?)',[todo,userId],
+        (err)=>{
+            if(err){console.error(err.message)}
+            else{console.log('todo created!')}}
 
-createUser('torvald','pingu')
+    )
+}
+
+
 
